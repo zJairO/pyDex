1ae7548f19f102ba94f384c6ec4849ebc6233515
Jairo <zjairo@outlook.com>
2020-12-08 16:31:05 -0600

    Update

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+import requests
+import json
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    r = requests.get('https://pokeapi.co/api/v2/pokedex/2')
+    pokemon = r.json()
+
+    res = requests.get('https://pokeapi.co/api/v2/pokemon/1')
+    poke = res.json()
+
+    return render_template('index.html', pokemon=pokemon, poke=poke, title='PyDex')
+
+app.run(debug = True, port=8000)
